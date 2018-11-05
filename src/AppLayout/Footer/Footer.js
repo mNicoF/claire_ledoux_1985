ba8b94a7da49525d6d84482b120f312648b365b6
mNicoF <m.nicolas.forget@outlook.com>
2018-11-05 15:31:56 +0100

    modification de la version pour build

--- src/AppLayout/Footer/Footer.js
+++ src/AppLayout/Footer/Footer.js
@@ -13,8 +13,8 @@ class Footer extends Component {
         <div className="Footer-left">
           <FontAwesomeIcon icon={['far', 'copyright']} /> Nicolas Forget
         </div>
-        <div className="Footer-right" title="Dernière mise à jour : 14/10/2018">
-          Version 0.1.0
+        <div className="Footer-right" title="Dernière mise à jour : 05/11/2018">
+          Version 0.1.1
         </div>
       </div>
     );
