e9e68d9cab488eb6d3e16da6f2d5ff83d61223dc
mNicoF <m.nicolas.forget@outlook.com>
2019-02-01 11:02:14 +0100

    add docs for gh-pages

--- docs/precache-manifest.c4f4173a502d088416f87bb817072716.js
+++ docs/precache-manifest.c4f4173a502d088416f87bb817072716.js
@@ -1,47 +1,47 @@
 self.__precacheManifest = [
   {
     "revision": "c26bbdb4571ecead1aa77e6029e1a338",
-    "url": "/static/media/tarifs_epil.c26bbdb4.PNG"
+    "url": "/docs/static/media/tarifs_epil.c26bbdb4.PNG"
   },
   {
     "revision": "da04dcf6cb1a8d78eda7",
-    "url": "/static/css/main.57931030.chunk.css"
+    "url": "/docs/static/css/main.57931030.chunk.css"
   },
   {
     "revision": "5b692e3163377864c0d70dde20212248",
-    "url": "/static/media/F25_Executive.5b692e31.otf"
+    "url": "/docs/static/media/F25_Executive.5b692e31.otf"
   },
   {
     "revision": "5d4b929147f45da094ac",
-    "url": "/static/js/1.5d4b9291.chunk.js"
+    "url": "/docs/static/js/1.5d4b9291.chunk.js"
   },
   {
     "revision": "229c360febb4351a89df",
-    "url": "/static/js/runtime~main.229c360f.js"
+    "url": "/docs/static/js/runtime~main.229c360f.js"
   },
   {
     "revision": "604a00965602521ccbb76e92f87a096e",
-    "url": "/static/media/Accueil_background.604a0096.JPG"
+    "url": "/docs/static/media/Accueil_background.604a0096.JPG"
   },
   {
     "revision": "da04dcf6cb1a8d78eda7",
-    "url": "/static/js/main.da04dcf6.chunk.js"
+    "url": "/docs/static/js/main.da04dcf6.chunk.js"
   },
   {
     "revision": "805ec453545141888828ac8e94b82092",
-    "url": "/static/media/tarifs_soins.805ec453.PNG"
+    "url": "/docs/static/media/tarifs_soins.805ec453.PNG"
   },
   {
     "revision": "2ec7fa44690298815b00d05b0dd28c6a",
-    "url": "/static/media/tarifs_dermo.2ec7fa44.PNG"
+    "url": "/docs/static/media/tarifs_dermo.2ec7fa44.PNG"
   },
   {
     "revision": "0aa17b82d8a0642d3627fd51ccd5e34e",
-    "url": "/static/media/contact.0aa17b82.PNG"
+    "url": "/docs/static/media/contact.0aa17b82.PNG"
   },
   {
     "revision": "5d4b929147f45da094ac",
-    "url": "/static/css/1.dcbbc469.chunk.css"
+    "url": "/docs/static/css/1.dcbbc469.chunk.css"
   },
   {
     "revision": "22044c3c1a5f30b0c8d8d4bbc45b40ff",
